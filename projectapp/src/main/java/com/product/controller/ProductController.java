4784f0b8890615ab2310674094a09730dbc8dc09
cyalla <cyalla@phoenix379618.private4.oaceng02phx.oraclevcn.com>
2024-04-04 09:01:31 +0000

    Initial commit

--- projectapp/src/main/java/com/product/controller/ProductController.java
+++ projectapp/src/main/java/com/product/controller/ProductController.java
@@ -40,8 +40,9 @@ public class ProductController {
     }
     
     @PostMapping
-    public ProductDTO createProduct(@RequestBody ProductDTO productDTO) {
-    	Product product = new Product();
+public ProductDTO createProduct(@RequestBody ProductDTO productDTO) {
+    try {
+        Product product = new Product();
         product.setName(productDTO.getName());
         String price = productDTO.getPrice();
         double priceDouble = Double.parseDouble(price);
@@ -49,10 +50,8 @@ public class ProductController {
         String tax = productDTO.getTax();
         double taxDouble = Double.parseDouble(tax);
         product.setTax(taxDouble);
-
-        double finalPrice = product.getPrice()* (1.0 + (product.getTax()/ 100.0));
+        double finalPrice = product.getPrice() * (1.0 + (product.getTax() / 100.0));
         product.setFinalPrice(finalPrice);
-
         Product savedProduct = productService.save(product);
         double priceSave = savedProduct.getPrice();
         String priceStr = String.valueOf(priceSave);
@@ -61,8 +60,16 @@ public class ProductController {
         double finalPriceSave = savedProduct.getFinalPrice();
         String finalPriceStr = String.valueOf(finalPriceSave);
         return new ProductDTO(savedProduct.getId(), savedProduct.getName(), priceStr, taxStr, finalPriceStr);
+    } catch (IllegalArgumentException e) {
+        // Handle illegal arguments exception
+        logger.error("Illegal arguments encountered: {}", e.getMessage());
+        return new ProductDTO.Builder().build();
+    } catch (Exception e) {
+        // Handle other exceptions
+        logger.error("An error occurred while creating the product:", e);
+        return new ProductDTO.Builder().build();
     }
-
+}
     @GetMapping("/{id}")
     public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id) {
         Product product = productService.findById(id)
